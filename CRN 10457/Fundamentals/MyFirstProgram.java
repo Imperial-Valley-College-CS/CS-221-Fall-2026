bccb1c6e3a45c8c6b7663cd09e9c5453b6631e4d
ortoct <104336410+ortoct@users.noreply.github.com>
2026-08-19 07:56:47 -0700

    Initial commit

---
+++ CRN 10457/Fundamentals/MyFirstProgram.java
@@ -0,0 +1,8 @@
+public class MyFirstProgram
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Welcome to CS 221");
+      System.out.println("Cool");
+   }
+}
---
+++ CRN 10457/Fundamentals/Variables.java
@@ -0,0 +1,8 @@
+public class Variables
+{
+   public static void main(String[] args)
+   {
+      int myAge = 38;
+      System.out.println( "I am " + myAge + " years old.");
+   }
+}
