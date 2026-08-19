a1e162a8862142d129d8ac8d3d3ee1b9cb1db7b8
ortoct <104336410+ortoct@users.noreply.github.com>
2026-08-19 12:54:42 -0700

    Added class on variables

---
+++ CRN 10285/Variables.java
@@ -0,0 +1,14 @@
+public class Variables
+{
+   public static void main(String[] args)
+   {
+      char firstI = 'O';
+      int age = 38;
+      String name = "Octavio";
+      double weight = 181.5;
+      boolean isOnlyChild = false;
+      
+      System.out.print("My name is " + name);
+      System.out.print("I am " + age + " years old." );
+   }
+}
